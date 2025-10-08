b8f5d50bd744c06d942186c179740e10f9473866
geeholiv <geovanna.oliveira1a@gmail.com>
2025-10-08 14:52:10 -0300

    Conversor Moedas

---
+++ ConversorMoedas/src/main/java/com/devsenai1A/ConversorMoedas/ConversorMoedasApplication.java
@@ -0,0 +1,13 @@
+package com.devsenai1A.ConversorMoedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorMoedasApplication.class, args);
+	}
+
+}
